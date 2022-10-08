44a21e37727bdd5ae76dd01a8135054bbca8c9d4
enyoukai <an.josephZero@gmail.com>
2022-10-08 12:50:05 -0700

    basic movement

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+	[SerializeField] private float speed = 5.0f;
+
+	private Rigidbody2D _RB;
+	private Vector2 movementVector;
+	private float rotationEuler;
+
+	void Awake()
+	{
+		_RB = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		_RB.velocity = movementVector * speed;
+		_RB.rotation = rotationEuler;
+	}
+
+	void Update()
+	{
+		movementVector = GetMovement();
+		rotationEuler = GetMouseAngle();
+	}
+
+	Vector2 GetMovement()
+	{
+		return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+	}
+
+	float GetMouseAngle()
+	{
+		Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 characterPosition = transform.position;
+		Vector2 characterToMouse = mousePosition - characterPosition;
+
+		float rotation = Mathf.Atan2(characterToMouse.y, characterToMouse.x);
+
+		return rotation * Mathf.Rad2Deg;
+	}
+}
