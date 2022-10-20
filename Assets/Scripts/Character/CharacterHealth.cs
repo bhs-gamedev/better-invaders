15e49e216d6809625ea1f21813cb1f3409df8849
Creonalia <hyper.pyromaniac@simplelogin.co>
2022-10-19 23:45:07 -0700

    added gameover menu

--- Assets/Scripts/Character/CharacterHealth.cs
+++ Assets/Scripts/Character/CharacterHealth.cs
@@ -6,10 +6,12 @@ public class CharacterHealth : MonoBehaviour
 {
     [SerializeField]
     int health = 3;
+    GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -21,5 +23,9 @@ public class CharacterHealth : MonoBehaviour
     public void Damage(int amount)
     {
         health -= amount;
+        if (health <= 0)
+        {
+            gameManager.GameOver();
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
     float lastSpawn = 0f;
+    bool isPlaying = true;
     public GameObject enemyPrefab;
+    public GameObject gameOverMenu;
+    GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         lastSpawn += Time.deltaTime;
+        if (isPlaying && lastSpawn > 3) {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
         float height = Camera.main.orthographicSize;
         float width = Camera.main.orthographicSize * Camera.main.aspect;
         Vector3[] positions = {
@@ -26,10 +38,21 @@ public class GameManager : MonoBehaviour
             new Vector3(-width, Random.Range(-height, height), 0)
         };
         Vector3 position = positions[Random.Range(0, 4)];
-        if (lastSpawn > 3) {
-            GameObject enemy = Instantiate(enemyPrefab, position, Quaternion.identity);
-            enemy.GetComponent<Enemy>().direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * 4;
-            lastSpawn = 0;
-        }
+        GameObject enemy = Instantiate(enemyPrefab, position, Quaternion.identity);
+        enemy.GetComponent<Enemy>().direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * 4;
+        lastSpawn = 0;
     }
+
+    public void GameOver()
+    {
+        isPlaying = false;
+        gameOverMenu.SetActive(true);
+        player.GetComponent<CharacterMovement>().enabled = false;
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
